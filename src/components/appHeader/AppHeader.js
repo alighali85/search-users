2c1a32361a9f7b02be895be7d6207a44b5c46b87
Ali Ghali <alifriends@live.com>
2020-07-20 20:55:19 +0200

    refactor(app): fix responnsive

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -3,13 +3,8 @@ import './app-header.css'
 
 
 export default function AppHeader(){
-    return (<>
-    <div className="header-wrapper" data-testid='app-header-test-id'>
-        <div id="lp-pom-block-12-color-overlay">
-                <img src="//d9hhrg4mnvzow.cloudfront.net/versicherung.dentolo.de/rundumschutz-premium-organic/4728e039-logo-dentolo-small-113x32.svg" alt="dentolo"/>
-                <p>Users platform</p>
-            </div>
+    return <div className="header-wrapper" data-testid='app-header-test-id'>
+        <img src="//d9hhrg4mnvzow.cloudfront.net/versicherung.dentolo.de/rundumschutz-premium-organic/4728e039-logo-dentolo-small-113x32.svg" alt="dentolo"/>
+        <br/><p>Users platform</p>
         </div>
-    </>
-    )
 }
