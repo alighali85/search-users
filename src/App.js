1fb409c01cdbab9da381df46f6938e19272c425e
Ali Ghali <alifriends@live.com>
2020-07-20 20:48:32 +0200

    feat(App): user app header and container to show users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from 'react'
+import Users from './containers/users'
+import AppHeader from './components/appHeader'
+import './app.css'
+
 
 function App() {
-    return ( < div className = "App" / > );
+    return <div className = "App">
+        <AppHeader />
+        <div className='search-container'>
+            <h2 className="search-container__title">Find a user by name</h2>
+            <Users />
+        </div>
+    </div>
 }
 
 export default App;
