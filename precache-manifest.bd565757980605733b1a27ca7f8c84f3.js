f7d36421a6f40a58fd59540a7333344cca893f47
Ali Ghali <alifriends@live.com>
2020-07-20 20:57:20 +0200

    Updates

---
+++ precache-manifest.bd565757980605733b1a27ca7f8c84f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a975dc6283f5c672a8854cc074b711ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "026b9ab4379010efc40b",
+    "url": "/static/css/main.e95fafd5.chunk.css"
+  },
+  {
+    "revision": "bcfdd415574b8401e364",
+    "url": "/static/js/2.0a3765cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a3765cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026b9ab4379010efc40b",
+    "url": "/static/js/main.5fa4a18e.chunk.js"
+  },
+  {
+    "revision": "eaed280571fbf8f8123c",
+    "url": "/static/js/runtime-main.18332197.js"
+  }
+]);
